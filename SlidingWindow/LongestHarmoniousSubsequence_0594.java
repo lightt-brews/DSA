4ebcf42e8e3979b4491fd649f7ad10b4ebfa6ff9
lightt-brews <tanmaysk20@gmail.com>
2026-06-24 03:18:32 +0530

    adding sliding window soln

---
+++ SlidingWindow/LongestHarmoniousSubsequence_0594.java
@@ -0,0 +1,30 @@
+import java.util.Hashtable;
+
+public class LongestHarmoniousSubsequence_0594 {
+    public static int findLHS(int[] nums) {
+        Hashtable<Integer,Integer> table = new Hashtable<>();
+        int t = 0;
+        for(int k= 0;k<nums.length;k++){
+            if(table.containsKey(nums[k])){
+                table.put(nums[k],table.get(nums[k])+1);
+            }
+            else{
+                table.put(nums[k],1);
+            }
+        }
+        for (Integer key : table.keySet()){
+            if (table.containsKey(key+1)){
+                t = Math.max(table.get(key+1)+ table.get(key),t);
+            }
+            else if(table.containsKey(key-1)){
+                t = Math.max(table.get(key-1)+ table.get(key),t);
+            }
+        }
+        return t;
+    }
+
+    public static void main(String[]args){
+        int[] nums = {1,3,2,2,5,2,3,7};
+        System.out.println(findLHS(nums));
+    }
+}
