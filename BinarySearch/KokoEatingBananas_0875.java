38a82042fbd5e21a472002f4e9fcb082033fc2a2
lightt-brews <tanmaysk20@gmail.com>
2026-06-26 06:30:15 +0530

    medium q on binary search

---
+++ BinarySearch/KokoEatingBananas_0875.java
@@ -0,0 +1,32 @@
+package BinarySearch;
+
+public class KokoEatingBananas_0875 {
+    public static int koko(int[] piles, int h){
+        int max = 1;
+        for(int i =0; i< piles.length;i++){
+            max  = Math.max(piles[i] , max);
+        }
+        int left = 1;
+        int right = max;
+        while(left<right){
+            int mid = left + (right-left) /2;
+            int totalHours = 0;
+            for(int i=0;i<piles.length;i++){
+                totalHours += ((piles[i]+mid-1)/mid);
+            }
+            if(totalHours<=h){
+                right = mid;
+            }
+            else if(totalHours>h){
+                left = mid +1;
+            }
+        }
+        return left;
+    }
+    
+    public static void main(String[] args){
+        int[] piles=  {3,6,7,11};
+        int h =8;
+        System.out.println(koko(piles,h));
+    }
+}
