85f79fe8ac400c42a13a3894e169e854ddc1270a
lightt-brews <tanmaysk20@gmail.com>
2026-06-27 11:04:05 +0530

    reversing a linked list

---
+++ LinkedList/Problem0206_ReverseLinkedList.java
@@ -0,0 +1,53 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Problem0206_ReverseLinkedList {
+    public static ListNode arrayToLinkedList(int[] arr) {
+        if (arr.length == 0) return null;
+
+        ListNode head = new ListNode(arr[0]);
+        ListNode current = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            current.next = new ListNode(arr[i]);
+            current = current.next;
+        }
+
+        return head;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev=null;
+        while(curr != null){
+            ListNode next;
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+
+    }
+
+    public static void main(String[] args){
+        int[] nums = {1,2,3,4,5};
+
+        ListNode l1 = arrayToLinkedList(nums);
+
+        Problem0206_ReverseLinkedList obj = new Problem0206_ReverseLinkedList();
+
+        ListNode result = obj.reverseList(l1);
+        while (result!=null){
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+
+    }
+}
